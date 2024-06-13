a3ee8d1e40fda2a53ac43487327dce803c4af18b
ter0ter0 <panicdepanic@gmail.com>
2024-06-13 15:44:21 +0900

    6/13 add

--- study2.sh
+++ study2.sh
@@ -39,14 +39,4 @@ echo "$total"
 done
 
 
-#99を作成します
-i="0"
-echo "99を作成します"
-while [ "$i" -lt "9" ]; do
-i="$(expr $i + 1)"
-echo "$iの段です"
-a="$(expr $i \* 1)"
-total="$(expr $a \* $num)"
-echo "$total"
-done
 
